a5a5b49d23a0346f1661efafd873c4121f4437bb
Shashank Mehra <smehra@mgageindia.com>
2018-04-23 16:32:23 +0530

    Update email and description

--- setup.py
+++ setup.py
@@ -27,14 +27,14 @@ REQUIRES = ["urllib3 >= 1.15", "six >= 1.10", "certifi", "python-dateutil"]
 setup(
     name=NAME,
     version=VERSION,
-    description="karix api",
-    author_email="apiteam@karix.com",
-    url="",
-    keywords=["Swagger", "karix api"],
+    description="Python helper for karix.io",
+    author_email="support@karix.com",
+    url="https://github.com/karixtech/karix-python",
+    keywords=["sms", "karix"],
     install_requires=REQUIRES,
     packages=find_packages(),
     include_package_data=True,
     long_description="""\
-    Overview  Karix API lets you interact with the Karix platform. It allows you to query your account, set up webhooks, send messages and buy phone numbers.   # noqa: E501
+    Karix API lets you interact with the Karix platform. It allows you to query your account, set up webhooks, send messages and buy phone numbers.   # noqa: E501
     """
 )
